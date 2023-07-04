96a991182311c434a828894129b4ac2845d4ecc4
Erdin Hermawan <erdinhermawann@gmail.com>
2023-07-04 12:36:34 +0700

    Added some media query as inherited widget

--- lib/src/flutter/context/context.dart
+++ lib/src/flutter/context/context.dart
@@ -4,27 +4,36 @@ extension ContextExtensions on BuildContext {
   // Returns the MediaQuery
   MediaQueryData get mediaQuery => MediaQuery.of(this);
 
+  /// Returns same as [MediaQuery.of(context).size]
+  Orientation get orientation => MediaQuery.orientationOf(this);
+
   /// Returns if Orientation is landscape
-  bool get isLandscape => mediaQuery.orientation == Orientation.landscape;
+  bool get isLandscape => orientation == Orientation.landscape;
 
   /// Returns if Orientation is portrait
-  bool get isPortrait => mediaQuery.orientation == Orientation.portrait;
+  bool get isPortrait => orientation == Orientation.portrait;
 
   /// Returns same as [MediaQuery.of(context).size]
-  Size get mediaQuerySize => mediaQuery.size;
+  Size get mediaQuerySize => MediaQuery.sizeOf(this);
 
   /// Returns same as [MediaQuery.of(context).padding]
-  EdgeInsets get padding => mediaQuery.padding;
+  EdgeInsets get padding => MediaQuery.paddingOf(this);
 
   /// Returns same as [MediaQuery.of(context).viewPadding]
-  EdgeInsets get viewPadding => mediaQuery.viewPadding;
+  EdgeInsets get viewPadding => MediaQuery.viewPaddingOf(this);
 
   /// Returns same as [MediaQuery.of(context).viewInsets]
-  EdgeInsets get viewInsets => mediaQuery.viewInsets;
+  EdgeInsets get viewInsets => MediaQuery.viewInsetsOf(this);
 
   /// Returns same as [MediaQuery.of(context).size.width]
   double get width => mediaQuerySize.width;
 
   /// Returns same as [MediaQuery.of(context).size.height]
   double get height => mediaQuerySize.height;
+
+  /// Returns same as [MediaQuery.of(context).brightness]
+  Brightness get brightness => MediaQuery.platformBrightnessOf(this);
+
+  /// Returns same as [MediaQuery.of(context).devicePixelRatio]
+  double get pixelRatio => MediaQuery.devicePixelRatioOf(this);
 }
